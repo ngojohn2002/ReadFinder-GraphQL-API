8313b2bdb6d2aa161831ba9a988f4c1f18c1a3f1
ngojohn2002 <ngojohn2002@yahoo.com>
2024-09-03 02:13:58 -0700

    added client/src/apolloClient.js to initialize and export an instance of Apollo Client

---
+++ client/src/apolloClient.js
@@ -0,0 +1,28 @@
+// src/apolloClient.js
+
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+// Replace with your GraphQL server endpoint
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
