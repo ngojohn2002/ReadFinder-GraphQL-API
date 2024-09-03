01ef8d9fb24a09b844de7ae9781e66bbb2418dc4
ngojohn2002 <ngojohn2002@yahoo.com>
2024-09-03 16:43:00 -0700

    Consolidated .gitignore and removed import API in SearchBooks.jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { SAVE_BOOK } from "../utils/mutations";
-import { searchGoogleBooks } from "../utils/API";
 import Auth from "../utils/auth";
 
 const SearchBooks = () => {
