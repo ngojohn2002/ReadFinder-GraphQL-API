483ee7030d03100149a4d649c1c71bf956d5c4d1
ngojohn2002 <ngojohn2002@yahoo.com>
2024-09-03 02:01:06 -0700

    updated client/src/App.jsx to ensure App.jsx file correctly sets up the router and wraps components with Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Outlet />
-    </>
+      <Routes>
+        <Route path="/" element={<SearchBooks />} />
+        <Route path="/saved" element={<SavedBooks />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </Router>
   );
 }
 
