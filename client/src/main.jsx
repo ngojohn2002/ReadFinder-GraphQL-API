918f116a8de3729ce71ce1001ed824e252890afa
ngojohn2002 <ngojohn2002@yahoo.com>
2024-09-03 00:55:27 -0700

    added bootstrap import to client/src/main.jsx; updated client/src/components/LoginForm.jsx component to use the LOGIN_USER mutation from mutations.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloProvider } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const client = new ApolloClient({
   uri: "/graphql",
